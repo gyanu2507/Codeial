0b5a959b2cf3343a8d75d156e3f6d31af43f33bf
gyanu2507 <81113356+gyanu2507@users.noreply.github.com>
2023-01-13 13:12:25 +0530

    Added SignIn SignUp page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,19 @@
 module.exports.profile = function (req, res) {
   res.end("<h1>User Profile</h1>");
 };
+
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codeial | Sign Up",
+  });
+};
+
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Codeial | Sign In",
+  });
+};
+
+module.exports.create = function (req, res) {};
+
+module.exports.createSession = function (req, res) {};
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usersConrtoller = require("../controllers/users_controller");
 
 router.get("/profile", usersConrtoller.profile);
-
+router.get("/sign-up", usersConrtoller.signUp);
+router.get("/sign-in", usersConrtoller.signIn);
 module.exports = router;
